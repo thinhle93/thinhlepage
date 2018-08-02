6c3dc79f66acceac8321a3e2363fb89b6073faa4
thinhle93 <thinhle93@gmail.com>
2018-08-02 13:58:26 -0700

    added video showing the updated sharepics project

--- js/script.js
+++ js/script.js
@@ -6,6 +6,10 @@ var sharebtn = document.getElementById('sharepics_btn');
 var sharemodal = document.getElementById('sharepics_modal');
 var sharecls = document.getElementById('shareclose');
 
+var sb = document.getElementById('sharepicsupdate_btn');
+var sm = document.getElementById('sharepicsupdate_modal');
+var sc = document.getElementById('shareupdateclose');
+
 btn.onclick = function() {
 	modal.style.display = "block";
 }
@@ -22,5 +26,14 @@ sharebtn.onclick = function(){
 sharecls.onclick = function() {
 	sharemodal.style.display = "none";
 	document.getElementById('sharepic_video').pause();
+}
+
+sb.onclick = function(){
+	sm.style.display = "block";
+}
+
+sc.onclick = function() {
+	sm.style.display = "none";
+	document.getElementById('sharepicsupdate_video').pause();
 }
 
