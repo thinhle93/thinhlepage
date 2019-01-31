63e1a1595b8b3542bf4297b14b4399611f8c534f
thinhle93 <thinhle93@gmail.com>
2019-01-30 16:45:15 -0800

    added DogCentral stuff to project section

--- js/script.js
+++ js/script.js
@@ -1,39 +1,41 @@
-var btn = document.getElementById('mybtn');
-var modal = document.getElementById('blackjack_modal');
-var btnclose = document.getElementById('btnclose');
+// var btn = document.getElementById('mybtn');
+// var modal = document.getElementById('blackjack_modal');
+// var btnclose = document.getElementById('btnclose');
 
-var sharebtn = document.getElementById('sharepics_btn');
-var sharemodal = document.getElementById('sharepics_modal');
-var sharecls = document.getElementById('shareclose');
+// var sharebtn = document.getElementById('sharepics_btn');
+// var sharemodal = document.getElementById('sharepics_modal');
+// var sharecls = document.getElementById('shareclose');
 
-var sb = document.getElementById('sharepicsupdate_btn');
-var sm = document.getElementById('sharepicsupdate_modal');
-var sc = document.getElementById('shareupdateclose');
+// var sb = document.getElementById('sharepicsupdate_btn');
+// var sm = document.getElementById('sharepicsupdate_modal');
+// var sc = document.getElementById('shareupdateclose');
 
-btn.onclick = function() {
-	modal.style.display = "block";
-}
+// btn.onclick = function() {
+// 	modal.style.display = "block";
+// }
 
-btnclose.onclick = function() {
-	modal.style.display = "none";
-	document.getElementById('video').pause();
-}
+// btnclose.onclick = function() {
+// 	modal.style.display = "none";
+// 	document.getElementById('video').pause();
+// }
 
-sharebtn.onclick = function(){
-	sharemodal.style.display = "block";
-}
+// sharebtn.onclick = function(){
+// 	sharemodal.style.display = "block";
+// }
 
-sharecls.onclick = function() {
-	sharemodal.style.display = "none";
-	document.getElementById('sharepic_video').pause();
-}
+// sharecls.onclick = function() {
+// 	sharemodal.style.display = "none";
+// 	document.getElementById('sharepic_video').pause();
+// }
 
-sb.onclick = function(){
-	sm.style.display = "block";
-}
+// sb.onclick = function(){
+// 	sm.style.display = "block";
+// }
 
-sc.onclick = function() {
-	sm.style.display = "none";
-	document.getElementById('sharepicsupdate_video').pause();
-}
+// sc.onclick = function() {
+// 	sm.style.display = "none";
+// 	document.getElementById('sharepicsupdate_video').pause();
+// }
+
+
 
